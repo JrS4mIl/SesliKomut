b12822f2dc43ccffea3de6f7d654d1a0b88dfebd
Yesil <yesil@gmail.com>
2024-01-15 15:33:58 +0300

    feat: microphone uyanma func

--- sesliAsistan.py
+++ sesliAsistan.py
@@ -1,30 +1,56 @@
+import random
+import time
 from gtts import gTTS
-import playsound
+from playsound import playsound
 import speech_recognition as sr
 import pyaudio
 import os
-import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 r=sr.Recognizer()
+
 class SesliAsistan:
-    def seslendirme(self,metin):
-        metin_seslendirme=gTTS(text=metin,lang='tr')
-        dosya=str(random.randint(0,1000000000))+".mp3"
+
+    def seslendirme(self, metin):
+        metin_seslendirme = gTTS(text=metin, lang="tr")
+        dosya = str(random.randint(0, 10000000000)) + ".mp3"
         metin_seslendirme.save(dosya)
         playsound(dosya)
         os.remove(dosya)
+
     def mikrofon(self):
         with sr.Microphone() as kaynak:
-            print('Sizi Dinliyorum...')
+            print("Sizi dinliyorum..")
             listen=r.listen(kaynak)
             ses=""
             try:
-                ses=r.recognize_google(listen,language='tr-TR')
+                ses=r.recognize_google(listen,language="tr-TR")
             except sr.UnknownValueError:
-                self.seslendirme('Anlasilmadi')
-            return  ses
+                self.seslendirme("ne dediğinizi anlayamadım")
+            return ses
 
-asistan=SesliAsistan()
+    def ses_karslik(self,gelen_Ses):
+        if(gelen_Ses in "merhaba"):
+            self.seslendirme("size de merhabalar")
+        elif(gelen_Ses in "nasılsın"):
+            self.seslendirme("iyiyim sizler nasılsınız")
+
+
+    def uyanma_fonksiyonu(self,gelen_Ses):
+        if(gelen_Ses in "hey siri"):
+            self.seslendirme("dinliyorum...")
+            ses=self.mikrofon()
+            if(ses!=""):
+                self.ses_karslik(ses)
+
+
+
+asistan = SesliAsistan()
+
 while True:
-    gelen_ses=asistan.mikrofon()
-    if(gelen_ses!=""):
-        print(gelen_ses)
+    gelen_Ses=asistan.mikrofon().lower()
+    if(gelen_Ses!=""):
+        print(gelen_Ses)
+        asistan.uyanma_fonksiyonu(gelen_Ses)
+
